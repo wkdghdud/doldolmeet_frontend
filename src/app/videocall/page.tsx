af0f3a5842eb3828d774e770f5d7a60687892d6a
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-11-13 22:11:38 +0900

    Comment: OpenVidu 관련 주석 작성

--- src/app/videocall/page.tsx
+++ src/app/videocall/page.tsx
@@ -5,17 +5,29 @@ import axios from "axios";
 import UserVideoComponent from "@/components/UserVideoComponent";
 
 const APPLICATION_SERVER_URL =
-  process.env.NODE_ENV === "production" ? "" : "https://demos.openvidu.io/";
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:5001/"; // TODO: 각자 실행한 백엔드 서버에 맞게 포트 수정 필요
 
 const VideoCall = () => {
+  /* 세션 구분용 ID */
   const [mySessionId, setMySessionId] = useState("SessionA");
+
+  /* 세션 참여자로서 보여지는 이름 */
   const [myUserName, setMyUserName] = useState(
     "Participant" + Math.floor(Math.random() * 100),
   );
+
+  /* 세션 */
   const [session, setSession] = useState(undefined);
+
+  /* Stream
+   * 스트림은 세션으로 흐르는 미디어 스트림이다.
+   * 참가자는 스트림을 게시(publish)할 수 있고, 동일한 세션의 다른 참가자들은 해당 스트림을 구독(subscribe)할 수 있다.
+   * */
   const [mainStreamManager, setMainStreamManager] = useState(undefined);
-  const [publisher, setPublisher] = useState(undefined);
-  const [subscribers, setSubscribers] = useState([]);
+  const [publisher, setPublisher] = useState(undefined); // 스트리머
+  const [subscribers, setSubscribers] = useState([]); // 시청자
+
+  /* Video 장치 */
   const [currentVideoDevice, setCurrentVideoDevice] = useState(undefined);
 
   useEffect(() => {
@@ -24,6 +36,8 @@ const VideoCall = () => {
     };
 
     window.addEventListener("beforeunload", handleBeforeUnload);
+
+    joinSession(); // TODO: 일단 카메라가 뜰 수 있도록 임시로 여기 넣어둔 코드인데 수정 필요
 
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
@@ -44,21 +58,29 @@ const VideoCall = () => {
     }
   };
 
+  /* Subscriber 삭제 */
   const deleteSubscriber = (streamManager) => {
     let newSubscribers = subscribers.filter((sub) => sub !== streamManager);
     setSubscribers(newSubscribers);
   };
 
+  /* Session 참여 */
   const joinSession = async () => {
     try {
+      // OpneVidu 객체 생성
       const ov = new OpenVidu();
+
+      // 세션 초기화
       const mySession = ov.initSession();
 
+      // 세션에 streamCreated 이벤트 등록
       mySession.on("streamCreated", (event) => {
-        const subscriber = mySession.subscribe(event.stream, undefined);
-        setSubscribers((prevSubscribers) => [...prevSubscribers, subscriber]);
+        // 새로운 stream을 받을 때마다
+        const subscriber = mySession.subscribe(event.stream, undefined); // stream을 subscribe해서 Subscriber 객체를 반환 받고
+        setSubscribers((prevSubscribers) => [...prevSubscribers, subscriber]); // subscribers 배열에 추가
       });
 
+      // 세션에 streamDestroyed 이벤트 등록
       mySession.on("streamDestroyed", (event) => {
         deleteSubscriber(event.stream.streamManager);
       });
@@ -106,6 +128,7 @@ const VideoCall = () => {
     }
   };
 
+  /* Session 참여 종료 */
   const leaveSession = () => {
     if (session) {
       session.disconnect();
@@ -118,6 +141,7 @@ const VideoCall = () => {
     setMyUserName("Participant" + Math.floor(Math.random() * 100));
   };
 
+  /* 카메라 전환 */
   const switchCamera = async () => {
     try {
       const devices = await session.getDevices();
@@ -149,11 +173,17 @@ const VideoCall = () => {
     }
   };
 
+  // 세션 생성 후 토큰 획득하기
   const getToken = async () => {
     const sessionId = await createSession(mySessionId);
     return await createToken(sessionId);
   };
 
+  /*
+   * Session 생성
+   * 세션은 오디오 스트림과 비디오 스트림을 교환할 수 있는 virtual room.
+   * 같은 세션에 연결된 사람끼리만 서로 연락할 수 있음.
+   * */
   const createSession = async (sessionId) => {
     const response = await axios.post(
       APPLICATION_SERVER_URL + "api/sessions",
@@ -165,7 +195,21 @@ const VideoCall = () => {
     return response.data;
   };
 
+  /*
+   * Token 생성
+   * 참여자가 세션에 접속하려면 토큰이 반드시 필요하다.
+   * 토큰은 Connection을 생성함으로써 획득할 수 있다.
+   * 토큰은 참여자에 대한 metadata를 제공할 수 있다.
+   * */
   const createToken = async (sessionId) => {
+    /*
+     * Connection:
+     * 커넥션은 세션에 참여하고 있는 하나의 참여자를 의미한다.
+     * 이 커넥션은 application server (즉, 우리의 Spring 서버)에서 초기화되어야 한다.
+     * 그리고 커넥션을 초기화해서 생성된 application client (즉, 우리의 React 프론트)에 전달되어야 한다.
+     * 이 토큰은 unauthorized 사용자가 세션에 접속하지 못하도록 막아준다.
+     * 한 번 커넥션을 획득한 클라이언트는 쭉 세션의 참여자로 인식된다.
+     * */
     const response = await axios.post(
       APPLICATION_SERVER_URL + "api/sessions/" + sessionId + "/connections",
       {},
