c946d8f9b22d922717d4020346db8338ebc390e5
jongho <whdgh135@gmail.com>
2023-11-20 15:03:07 +0900

    Feat: 채팅 초안 완성

--- src/components/ShowChat/index.tsx
+++ src/components/ShowChat/index.tsx
@@ -1,36 +1,32 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
-import { Button } from "@mui/material";
+import { Button, Paper, TextField, Typography } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 const ShowChat = () => {
-  const roomId = "ca37eb4e-2494-44c0-ae1a-7da8d45ef179";
-  const sender = "park";
-
+  const roomId = "11beccc3-7712-4238-bc89-39aa442c353c";
   const [message, setMessage] = useState("");
-
+  const [messages, setMessages] = useState([]);
+  const [sender, setSender] = useState<string | null>("");
   const sock = new SockJS("http://localhost:8080/ws-stomp");
   const stompClient = Stomp.over(sock);
-  let room = "";
 
-  // axios.get("/chat/room/" + roomId).then((response) => {
-  //   room = response.data;
-  // });
-
-  connect();
+  useEffect(() => {
+    connect();
+  }, []);
 
   function connect() {
     stompClient.connect(
       {},
       function (frame) {
-        console.log("Connected: " + frame);
-
-        // 특정 토픽 구독
         stompClient.subscribe(`/sub/chat/room/${roomId}`, function (message) {
-          console.log("Received message: " + message.body);
-          // 여기에서 수신한 메시지를 처리하는 로직을 추가할 수 있습니다.
+          const receivedMessage = JSON.parse(message.body);
+          setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+          console.log(messages);
         });
+
         stompClient.send(
           "/pub/chat/message",
           {},
@@ -43,17 +39,78 @@ const ShowChat = () => {
     );
   }
 
+  function sendMessage(e) {
+    e.preventDefault();
+    stompClient.send(
+      "/pub/chat/message",
+      {},
+      JSON.stringify({
+        type: "TALK",
+        roomId: roomId,
+        sender: sender,
+        message: message,
+      }),
+    );
+    setMessage("");
+  }
+
   return (
-    <>
-      <input type="text" />
-      <Button
-        onClick={() => {
-          console.log(123);
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <Paper
+        elevation={3}
+        style={{
+          padding: "20px",
+          maxWidth: "600px",
+          width: "100%",
+          height: "40%",
+          marginBottom: "20px",
         }}
       >
-        클릭
-      </Button>
-    </>
+        <Typography variant="h4" gutterBottom>
+          채팅방
+        </Typography>
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          {messages.map((msg, index) => (
+            <li key={index} style={{ marginBottom: "8px" }}>
+              <Typography variant="body1" style={{ fontWeight: "bold" }}>
+                {msg.sender}: {msg.message}
+              </Typography>
+            </li>
+          ))}
+        </ul>
+      </Paper>
+      <Paper
+        elevation={3}
+        style={{ padding: "20px", maxWidth: "600px", width: "100%" }}
+      >
+        <div style={{ display: "flex" }}>
+          <TextField
+            label="아이디"
+            variant="outlined"
+            value={sender}
+            onChange={(e) => setSender(e.target.value)}
+            style={{ marginRight: "10px" }}
+          />
+          <TextField
+            label="메시지 입력"
+            variant="outlined"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            style={{ flex: 1 }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            endIcon={<SendIcon />}
+            onClick={sendMessage}
+          >
+            전송
+          </Button>
+        </div>
+      </Paper>
+    </div>
   );
 };
 
