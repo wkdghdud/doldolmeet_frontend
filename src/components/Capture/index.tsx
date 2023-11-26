6f6da576f8143e988772da68c08fee9aaa961eb0
jongho <whdgh135@gmail.com>
2023-11-26 17:27:09 +0900

    Feat: 캡쳐 사진 s3에 업로드 기능 구현

--- src/components/Capture/index.tsx
+++ src/components/Capture/index.tsx
@@ -1,4 +1,6 @@
 import html2canvas from "html2canvas";
+import { useCallback } from "react";
+import { backend_api } from "@/utils/api";
 
 const Capture = () => {
   const onCapture = () => {
@@ -7,7 +9,9 @@ const Capture = () => {
     if (targetElement) {
       html2canvas(targetElement)
         .then((canvas) => {
-          onSavaAs(canvas.toDataURL("image/png"), "image-download.png");
+          // onSavaAs(canvas.toDataURL("image/png"), "image-download.png");
+          const imageDataUrl = canvas.toDataURL("image/png");
+          uploadImage(imageDataUrl);
         })
         .catch((error) => {
           console.error("html2canvas error:", error);
@@ -16,6 +20,41 @@ const Capture = () => {
       console.error("Target element not found");
     }
   };
+
+  const uploadImage = (imageDataUrl) => {
+    console.log("🎲🎲🎲🎲🎲🎲🎲", imageDataUrl);
+
+    const blobImage = dataURLtoBlob(imageDataUrl);
+    // Blob을 파일로 변환
+    const imageFile = new File([blobImage], "image.png", { type: "image/png" });
+
+    const formData = new FormData();
+    formData.append("multipartFile", imageFile);
+    backend_api()
+      .post("/s3/file", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((response) => {
+        console.log("Image uploaded successfully:", formData);
+      })
+      .catch((error) => {
+        console.error("Image upload failed:", error);
+      });
+  };
+
+  function dataURLtoBlob(dataURL) {
+    let arr = dataURL.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new Blob([u8arr], { type: mime });
+  }
 
   const onSavaAs = (uri, filename) => {
     console.log("onSavaAs");
