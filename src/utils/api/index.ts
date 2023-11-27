b77625ee55c3f4dbd68473160d055297439fa195
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-11-27 14:26:24 +0900

    Chore: ec2 배포 테스트용 백엔드 IP 임시 변경

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -3,7 +3,8 @@ import { getSession } from "next-auth/react";
 
 const SPRING_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.doldolmeet.shop"
+    ? // ? "https://api.doldolmeet.shop"
+      "http://43.201.87.133:8080"
     : "http://43.201.87.133:8080";
 
 export const backend_api = () => {
@@ -52,5 +53,6 @@ export const openvidu_api = axios.create({
 
 export const WS_STOMP_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.doldolmeet.shop/ws-stomp"
+    ? // ? "https://api.doldolmeet.shop/ws-stomp"
+      "http://43.201.87.133:8080/ws-stomp"
     : "http://43.201.87.133:8080/ws-stomp";
