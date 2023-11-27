833b6d6b82fba4eca70381c45c9f1b6c26589036
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-11-27 19:37:17 +0900

    Test: OpenVidu Composed 녹화 테스트

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -70,7 +70,7 @@ export default function App() {
           name: "room-" + mySessionId + "_memberId-" + myUserName,
           hasAudio: true,
           hasVideo: true,
-          outputMode: "INDIVIDUAL",
+          outputMode: "COMPOSED",
           // recordingLayout: "CUSTOM",
           // customLayout: "mySimpleLayout",
           // resolution: "1280x720",
@@ -397,8 +397,8 @@ export default function App() {
               onClick={leaveSession}
               value="Leave session"
             />
-            {/*<button onClick={startRecording}>Start Recording</button>*/}
-            {/*<button onClick={stopRecording}>Stop Recording</button>*/}
+            <button onClick={startRecording}>Start Recording</button>
+            <button onClick={stopRecording}>Stop Recording</button>
             {/*<Recording></Recording>*/}
             <button onClick={onCapture}>캡쳐</button>
             <input
