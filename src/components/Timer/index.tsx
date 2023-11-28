61aa65db0d556da290a0b4d1d39ac6c078523d5c
BellPumpkin <whdgh135@gmail.com>
2023-11-29 00:57:00 +0900

    Chore: 1대1 영상 통화 팬 입장에서 시간 조금 남았을 때 EndAlertBar UI 디자인 변경 테스트

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -13,7 +13,7 @@ const EndAlertBar = ({ open, handleClose }: Props) => {
       autoHideDuration={3000}
       onClose={handleClose}
       anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      sx={{ marginTop: "2.5%", marginRight: "5%", width: "20%" }}
+      sx={{ marginTop: "2.5%", marginRight: "10%", width: "20%" }}
     >
       <Alert
         icon={<NotificationsActiveIcon fontSize="inherit" />}
