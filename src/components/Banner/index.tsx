ffb6be66bb60890536b90703962e3df30a9b805a
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-11-29 23:26:54 +0900

    Style: 홈페이지 배너 수정

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,9 +3,7 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 
-const imageUrls: string[] = [
-  "https://pbs.twimg.com/media/F6GjqPia0AAHxaN.jpg:large",
-];
+const imageUrls: string[] = ["/newjeans_banner.png"];
 export default function Banner() {
   return (
     <Carousel>
