f50505087db00b1ac579a3dfc390418feb230421
janghoyeong <ghdud4653@naver.com>
2023-11-30 15:14:44 +0900

    Style: end page 캡쳐, 녹화본 UI 수정

--- pages/end-fanmeeting/[userName]/[fanMeetingId].tsx
+++ pages/end-fanmeeting/[userName]/[fanMeetingId].tsx
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { backend_api } from "@/utils/api";
-import { Button, Grid, IconButton, Typography } from "@mui/material";
+import { Button, Grid, IconButton, Paper, Typography } from "@mui/material";
 import { GetApp, Twitter } from "@mui/icons-material";
+import GradientButton from "@/components/GradientButton";
 
 const EndFanMeetingPage = () => {
   const router = useRouter();
@@ -11,6 +12,10 @@ const EndFanMeetingPage = () => {
   const [captures, setCaptures] = useState([]);
 
   const s3Addr = "https://s3.ap-northeast-2.amazonaws.com/doldolmeet.test/";
+
+  const joinMemoryRoom = async () => {
+    await router.push(`/my-page/${userName}/${fanMeetingId}`);
+  };
 
   useEffect(() => {
     // fanMeetingId가 유효한 경우에만 API 호출 수행
@@ -71,35 +76,32 @@ const EndFanMeetingPage = () => {
         <Typography variant="h1" align="center" gutterBottom>
           팬미팅이 종료되었습니다.
         </Typography>
+        <GradientButton
+          variant="contained"
+          disableElevation
+          sx={{ px: 3, borderRadius: 10 }}
+          onClick={joinMemoryRoom}
+        >
+          추억 보관함
+        </GradientButton>
       </Grid>
       <Grid item xs={12} md={6}>
-        <video width="100%" controls>
-          <source
-            src="https://youngeui-in-jungle.store/openvidu/recordings/b563a3d2-2300-412a-b7ca-b06b0df972ea/b563a3d2-2300-412a-b7ca-b06b0df972ea.mp4"
-            type="video/mp4"
-          />
-        </video>
-      </Grid>
-      <Grid item xs={12} md={6}>
+        <Typography variant="h2" align="center" gutterBottom>
+          녹화된 영상
+        </Typography>
         <Grid container spacing={1}>
-          {captures.length > 0 ? (
+          {captures.length > 0 /* Todo: captures를 videos로 변경해야됨 */ ? (
             captures.map((cap, i) => (
               <Grid item xs={6} sm={6} key={i}>
-                <div style={{ marginBottom: "20px" }}>
-                  <Typography variant="body1">
-                    Capture ID: {cap.captureId}
-                  </Typography>
+                <Paper elevation={3} style={{ padding: "10px" }}>
                   <div>
-                    <img
-                      src={s3Addr + cap.captureUrl}
-                      alt={`Capture ${i}`}
-                      style={{
-                        width: "100%",
-                        height: "auto",
-                        marginBottom: "10px",
-                        maxHeight: "150px",
-                      }}
-                    />
+                    <video width="100%" controls>
+                      <source
+                        src="https://youngeui-in-jungle.store/openvidu/recordings/b563a3d2-2300-412a-b7ca-b06b0df972ea/b563a3d2-2300-412a-b7ca-b06b0df972ea.mp4"
+                        // src={`https://youngeui-in-jungle.store/openvidu/recordings/${cap.videoId}/${cap.videoId}.mp4`}
+                        type="video/mp4"
+                      />
+                    </video>
                     <IconButton onClick={() => imgDownLoad(cap.captureUrl)}>
                       <GetApp />
                     </IconButton>
@@ -109,7 +111,51 @@ const EndFanMeetingPage = () => {
                       <Twitter />
                     </IconButton>
                   </div>
-                </div>
+                </Paper>
+              </Grid>
+            ))
+          ) : (
+            <Typography variant="body1">
+              No captures found for this fanMeetingId.
+            </Typography>
+          )}
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Typography variant="h2" align="center" gutterBottom>
+          캡쳐된 이미지
+        </Typography>
+        <Grid container spacing={1}>
+          {captures.length > 0 ? (
+            captures.map((cap, i) => (
+              <Grid item xs={6} sm={6} key={i}>
+                <Paper elevation={3} style={{ padding: "10px" }}>
+                  <div>
+                    {/*<Typography variant="body1">*/}
+                    {/*  Capture ID: {cap.captureId}*/}
+                    {/*</Typography>*/}
+                    <div>
+                      <img
+                        src={s3Addr + cap.captureUrl}
+                        alt={`Capture ${i}`}
+                        style={{
+                          width: "100%",
+                          height: "auto",
+                          marginBottom: "33px",
+                          maxHeight: "150px",
+                        }}
+                      />
+                      <IconButton onClick={() => imgDownLoad(cap.captureUrl)}>
+                        <GetApp />
+                      </IconButton>
+                      <IconButton
+                        onClick={() => shareTwitter(s3Addr + cap.captureUrl)}
+                      >
+                        <Twitter />
+                      </IconButton>
+                    </div>
+                  </div>
+                </Paper>
               </Grid>
             ))
           ) : (
