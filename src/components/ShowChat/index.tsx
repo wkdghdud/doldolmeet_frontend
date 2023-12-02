f1920669a2650cd6b79a94a1dc0500884ee6faec
BellPumpkin <whdgh135@gmail.com>
2023-12-03 00:42:35 +0900

    Test: 채팅 번역 기능 테스트

--- src/components/ShowChat/index.tsx
+++ src/components/ShowChat/index.tsx
@@ -2,19 +2,60 @@
 import React, { useEffect, useRef, useState } from "react";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
-import { IconButton, TextField } from "@mui/material";
+import { Button, IconButton, MenuItem, Select, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import useJwtToken from "@/hooks/useJwtToken";
-import { WS_STOMP_URL } from "@/utils/api";
+import { backend_api, WS_STOMP_URL } from "@/utils/api";
 import ChatBalloon from "@/components/chat/ChatBalloon";
 import { Box } from "@mui/system";
+import { Theme, useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import FormControl from "@mui/material/FormControl";
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+const SUPPORTED_TARGETS = [
+  { code: "ja", label: "Japanese" },
+  { code: "en", label: "English" },
+  { code: "ko", label: "Korean" },
+];
 const ShowChat = ({ roomId }: { roomId: string | undefined }) => {
   const [message, setMessage] = useState<any>("");
   const [messages, setMessages] = useState<any[]>([]);
   const [sender, setSender] = useState<string | null>("");
   const [sock, setSock] = useState<any>(null);
   const [stompClient, setStompClient] = useState<any>(null);
+  const [text, setText] = useState("");
 
   const messagesRef = useRef<HTMLElement | null>(null);
   const token = useJwtToken();
@@ -90,6 +131,29 @@ const ShowChat = ({ roomId }: { roomId: string | undefined }) => {
     }
   };
 
+  const [target, setTarget] = useState("ko"); // 기본값은 "ko"
+  const toggleTarget = () => {
+    // 현재 target의 인덱스를 찾아서 다음 target으로 변경
+    const currentIndex = SUPPORTED_TARGETS.findIndex(
+      (item) => item.code === target,
+    );
+    const nextIndex = (currentIndex + 1) % SUPPORTED_TARGETS.length;
+    setTarget(SUPPORTED_TARGETS[nextIndex].code);
+  };
+
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value,
+    );
+  };
+
   return (
     <Box
       sx={{
@@ -102,6 +166,52 @@ const ShowChat = ({ roomId }: { roomId: string | undefined }) => {
         borderRadius: 2,
       }}
     >
+      <Select
+        value={target}
+        onChange={(e) => setTarget(e.target.value)}
+        variant="standard"
+        style={{ marginTop: "8px" }}
+      >
+        {SUPPORTED_TARGETS.map((item) => (
+          <MenuItem key={item.code} value={item.code}>
+            {item.label}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <div>
+        <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+          <Select
+            displayEmpty
+            value={personName}
+            onChange={handleChange}
+            input={<OutlinedInput />}
+            renderValue={(selected) => {
+              if (selected.length === 0) {
+                return <em>Placeholder</em>;
+              }
+
+              return selected.join(", ");
+            }}
+            MenuProps={MenuProps}
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            <MenuItem disabled value="">
+              <em>Placeholder</em>
+            </MenuItem>
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, personName, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
       <Box
         ref={messagesRef}
         sx={{
@@ -119,6 +229,7 @@ const ShowChat = ({ roomId }: { roomId: string | undefined }) => {
                 key={index}
                 sender={msg.sender}
                 message={msg.message}
+                isLanaguage={target}
               />
             ),
         )}
@@ -161,6 +272,7 @@ const ShowChat = ({ roomId }: { roomId: string | undefined }) => {
           ),
         }}
       />
+      {/*<Button onClick={toggleTarget}>Toggle Target</Button>*/}
     </Box>
   );
 };
