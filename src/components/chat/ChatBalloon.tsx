05dec48c687843e53cd1843c58216ba2a9a8e52b
janghoyeong <ghdud4653@naver.com>
2023-12-06 13:49:48 +0900

    Fix: 유튜브 링크 변경, 채팅 쇼츠도 뜨게 변경

--- src/components/chat/ChatBalloon.tsx
+++ src/components/chat/ChatBalloon.tsx
@@ -23,8 +23,7 @@ export default function ChatBalloon({
 }) {
   const createMarkup = useMemo(() => {
     return (text) => {
-      const youtubeRegex =
-        /(https?:\/\/(?:www\.)?youtube\.com\/shorts\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/g;
+      const youtubeRegex = /youtube\.com\/shorts\/([a-zA-Z0-9_-]{11})/;
       const replacedText = text.replace(
         youtubeRegex,
         '<iframe width="100%" height="auto" src="https://www.youtube.com/embed/$2" frameborder="0" allowfullscreen></iframe>',
