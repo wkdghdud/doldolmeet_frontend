bbd01c43c2e96eb1eff7517b44519a6e2bd2cdd1
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-12-07 08:02:12 +0900

    Feat: 게임 우승자 팝업 추가

---
+++ src/components/WinnerDialog/index.tsx
@@ -0,0 +1,84 @@
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Stack,
+} from "@mui/material";
+import { Publisher, StreamManager, Subscriber } from "openvidu-browser";
+import IdolStreamView from "@/components/meeting/IdolStreamView";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import React, { useEffect } from "react";
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+  winnerName: string;
+  fanStream: StreamManager | Subscriber | Publisher | undefined;
+}
+
+const WinnerDialog = ({ open, onClose, winnerName, fanStream }: Props) => {
+  // @ts-ignore
+  const audio = new Audio("/mp3/fanfare.mp3");
+
+  useEffect(() => {
+    if (open) {
+      audio.play();
+    }
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, [open]);
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      sx={{
+        "& .MuiDialogContent-root": {
+          padding: 2,
+        },
+        "& .MuiDialogActions-root": {
+          padding: 2,
+        },
+      }}
+    >
+      <Stack
+        direction={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <DialogTitle variant={"h3"}>
+          🥳 {winnerName}님이 정답을 맞추셨어요!
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </Stack>
+      <DialogContent>
+        {fanStream && (
+          <IdolStreamView name={winnerName} streamManager={fanStream} />
+        )}
+      </DialogContent>
+      <DialogContentText textAlign={"center"} variant={"h5"}>
+        정답을 맞추신 분께는 미공개 포토카드가 제공됩니다!
+      </DialogContentText>
+      <DialogActions></DialogActions>
+    </Dialog>
+  );
+};
+
+export default WinnerDialog;
