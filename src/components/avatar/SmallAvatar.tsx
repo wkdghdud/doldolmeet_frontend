22c5dbd13008bb00e41ef93622f52ce3ffdb6fa9
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-12-08 17:26:44 +0900

    Feat: 로그인된 경우 유저의 프로필 아이콘이 보이고, 프로필 아이콘을 클릭하면 메뉴바가 보이는 기능 추가

--- src/components/avatar/SmallAvatar.tsx
+++ src/components/avatar/SmallAvatar.tsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { Avatar, Box, IconButton } from "@mui/material";
 
-const SmallAvatar = ({ imgSrc }: { imgSrc?: string }) => {
-  const [anchorEl2, setAnchorEl2] = useState(null);
-  const handleClick2 = (event) => {
-    setAnchorEl2(event.currentTarget);
-  };
+interface Props {
+  imgSrc?: string;
+  anchorEl2?: (EventTarget & Element) | null;
+  setAnchorEl2?: Dispatch<SetStateAction<(EventTarget & Element) | null>>;
+}
 
+const SmallAvatar = ({ imgSrc, anchorEl2, setAnchorEl2 }: Props) => {
+  const handleClickAnchor = (event) => {
+    if (setAnchorEl2) {
+      setAnchorEl2(event.currentTarget);
+    }
+  };
   return (
     <Box>
       <IconButton
@@ -20,7 +26,7 @@ const SmallAvatar = ({ imgSrc }: { imgSrc?: string }) => {
             color: "primary.main",
           }),
         }}
-        onClick={handleClick2}
+        onClick={handleClickAnchor}
       >
         <Avatar
           src={imgSrc ?? "/default_avatar.png"}
