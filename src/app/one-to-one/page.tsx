3fb95de418b9e8d2906e0c6e5b58b14543d990e1
janghoyeong <ghdud4653@naver.com>
2023-12-08 17:32:53 +0900

    Fix: 컴포넌트 언마운트시 leavesession 되도록 코드 수정

--- src/app/one-to-one/page.tsx
+++ src/app/one-to-one/page.tsx
@@ -400,35 +400,35 @@ const OneToOnePage = () => {
     setMyConnection(undefined);
   };
 
-  useEffect(() => {
-    // 브라우저의 페이지 이동이나 닫힘 감지를 위한 핸들러
-    const handlePageChange = () => {
-      leaveSession();
-    };
-
-    // popstate 이벤트 리스너 등록: 브라우저 내부 이동 감지 (예: 뒤로 가기)
-    window.addEventListener("popstate", handlePageChange);
-
-    // beforeunload 이벤트 리스너 등록: 페이지 닫힘 감지
-    window.addEventListener("beforeunload", handlePageChange);
-
-    // 컴포넌트 언마운트 시 이벤트 리스너 제거
-    return () => {
-      window.removeEventListener("popstate", handlePageChange);
-      window.removeEventListener("beforeunload", handlePageChange);
-    };
-  }, [leaveSession]);
-
   // useEffect(() => {
-  //   const handleBeforeUnload = (event) => {
+  //   // 브라우저의 페이지 이동이나 닫힘 감지를 위한 핸들러
+  //   const handlePageChange = () => {
   //     leaveSession();
   //   };
-  //   window.addEventListener("beforeunload", handleBeforeUnload);
   //
+  //   // popstate 이벤트 리스너 등록: 브라우저 내부 이동 감지 (예: 뒤로 가기)
+  //   window.addEventListener("popstate", handlePageChange);
+  //
+  //   // beforeunload 이벤트 리스너 등록: 페이지 닫힘 감지
+  //   window.addEventListener("beforeunload", handlePageChange);
+  //
+  //   // 컴포넌트 언마운트 시 이벤트 리스너 제거
   //   return () => {
-  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //     window.removeEventListener("popstate", handlePageChange);
+  //     window.removeEventListener("beforeunload", handlePageChange);
   //   };
   // }, [leaveSession]);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      leaveSession();
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [leaveSession]);
 
   const joinNextRoom = async (sessionId: string, nextRoomType: string) => {
     if (nextRoomType === "gameRoom") {
@@ -491,6 +491,11 @@ const OneToOnePage = () => {
     setFilterPopupOpen(false);
   };
 
+  useEffect(() => {
+    return () => {
+      leaveSession();
+    };
+  }, [leaveSession]);
   return (
     <Grid container spacing={2}>
       <Grid
