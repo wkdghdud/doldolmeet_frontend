30786a2979b0e1a6f24c1551f3d7f6d023acd746
janghoyeong <ghdud4653@naver.com>
2023-12-10 03:15:36 +0900

    Test: 녹화본 썸네일 변경 추가 Test

--- pages/end-fanmeeting/[userName]/[fanMeetingId].tsx
+++ pages/end-fanmeeting/[userName]/[fanMeetingId].tsx
@@ -361,6 +361,8 @@ const EndFanMeetingPage = () => {
                   </Stack>
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </Carousel>
