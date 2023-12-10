5519a30649320a8979c9508289939f32c8ececf0
janghoyeong <ghdud4653@naver.com>
2023-12-11 00:13:36 +0900

    Fix: 아이돌 게임 페이지 버튼 ui 수정

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -251,15 +251,17 @@ const SingGamePage = ({
         )}
 
         {role === Role.IDOL && (
-          <>
+          <Stack direction="column" spacing={2}>
             <GradientButton onClick={startGame}>
-              게임 시작 버튼 활성화
+              🎬 게임 시작 버튼 활성화🎬
             </GradientButton>
-            <GradientButton onClick={send_replay}>다시 들려주기</GradientButton>
+            <GradientButton onClick={send_replay}>
+              🎶다시 들려주기🎶
+            </GradientButton>
             <GradientButton onClick={signalGoToEndPage}>
-              종료 페이지로 보내기
+              👋🏿종료 페이지로 보내기👋🏿
             </GradientButton>
-          </>
+          </Stack>
         )}
       </Box>
       {showAllIdolEnteredmodal && (
