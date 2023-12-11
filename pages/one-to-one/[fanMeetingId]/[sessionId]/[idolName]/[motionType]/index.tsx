413565c551cbfb23925b6b1da391e64005ba1d52
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-12-11 13:56:09 +0900

    Revert "Test: fetchFanToFanMeeting가 팬의 입장 지연에 미치는 영향 테스트"

--- pages/one-to-one/[fanMeetingId]/[sessionId]/[idolName]/[motionType]/index.tsx
+++ pages/one-to-one/[fanMeetingId]/[sessionId]/[idolName]/[motionType]/index.tsx
@@ -156,10 +156,10 @@ const OneToOnePage = () => {
         await fetchSSE_idol();
         await joinSession();
       } else if (role === Role.FAN) {
-        // const fanToFanMeeting = await fetchFanToFanMeeting(fanMeetingId);
-        // setChatRoomId(fanToFanMeeting?.chatRoomId);
-        await joinSession("");
         await fetchSSE();
+        const fanToFanMeeting = await fetchFanToFanMeeting(fanMeetingId);
+        setChatRoomId(fanToFanMeeting?.chatRoomId);
+        await joinSession(fanToFanMeeting?.chatRoomId);
       } else {
         await joinSession();
       }
@@ -285,29 +285,30 @@ const OneToOnePage = () => {
           },
         });
       } else if (role === Role.FAN) {
-        await mySession.connect(token, {
-          clientData: JSON.stringify({
-            role: role,
-            fanMeetingId: fanMeetingId,
-            userName: userName,
-            type: "idolRoom",
-            chatRoomId: _chatRoomId,
-            nickname: myNickName,
-            idolName: idolName,
-          }),
-          kurentoOptions: {
-            allowedFilters: [
-              "FaceOverlayFilter",
-              "ChromaFilter",
-              "GStreamerFilter",
-            ],
-          },
-        });
-        // .then(async () => {
-        //   if (role === Role.FAN) {
-        //     await startRecording();
-        //   }
-        // });
+        await mySession
+          .connect(token, {
+            clientData: JSON.stringify({
+              role: role,
+              fanMeetingId: fanMeetingId,
+              userName: userName,
+              type: "idolRoom",
+              chatRoomId: _chatRoomId,
+              nickname: myNickName,
+              idolName: idolName,
+            }),
+            kurentoOptions: {
+              allowedFilters: [
+                "FaceOverlayFilter",
+                "ChromaFilter",
+                "GStreamerFilter",
+              ],
+            },
+          })
+          .then(async () => {
+            if (role === Role.FAN) {
+              await startRecording();
+            }
+          });
       }
 
       await ov.getUserMedia({
