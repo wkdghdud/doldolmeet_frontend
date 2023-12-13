a7e90282dc5a4b4520965cafcc51c1329e54f33e
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2023-12-14 02:10:48 +0900

    Fix: 팬미팅 시간 55초로 변경

--- pages/one-to-one/[fanMeetingId]/[sessionId]/[idolName]/[motionType]/index.tsx
+++ pages/one-to-one/[fanMeetingId]/[sessionId]/[idolName]/[motionType]/index.tsx
@@ -132,7 +132,7 @@ const OneToOnePage = () => {
   const [filterPopupOpen, setFilterPopupOpen] = useState(false);
 
   /* 남은 통화 시간 */
-  const [timeLimit, setTimeLimit] = useState(60);
+  const [timeLimit, setTimeLimit] = useState(55);
 
   /* EventSource */
   const [fanEventSource, setFanEventSource] = useState<EventSource | null>(
